4c8e0b95204fb5670835fc32c984f0bc86195f1e
David Muto (pseudomuto) <david.muto@gmail.com>
2018-02-19 20:26:48 -0500

    Add services to the example

--- examples/jsonator/main.go
+++ examples/jsonator/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/golang/protobuf/protoc-gen-go/plugin"
 	"github.com/pseudomuto/protokit"
 
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -26,28 +27,52 @@ func (p *plugin) Generate(req *plugin_go.CodeGeneratorRequest) (*plugin_go.CodeG
 		files[i] = newFile(d)
 	}
 
-	data, err := json.Marshal(files)
-	if err != nil {
+	buf := new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	enc.SetIndent("", "  ")
+
+	if err := enc.Encode(files); err != nil {
 		return nil, err
 	}
 
 	resp := new(plugin_go.CodeGeneratorResponse)
 	resp.File = append(resp.File, &plugin_go.CodeGeneratorResponse_File{
 		Name:    proto.String("output.json"),
-		Content: proto.String(string(data)),
+		Content: proto.String(buf.String()),
 	})
 
 	return resp, nil
 }
 
 type file struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	Services    []*service `json:"services"`
 }
 
 func newFile(fd *protokit.FileDescriptor) *file {
+	svcs := make([]*service, len(fd.GetServices()))
+	for i, sd := range fd.GetServices() {
+		svcs[i] = newService(sd)
+	}
+
 	return &file{
 		Name:        fmt.Sprintf("%s.%s", fd.GetPackage(), fd.GetName()),
 		Description: fd.GetDescription(),
+		Services:    svcs,
 	}
+}
+
+type service struct {
+	Name    string   `json:"name"`
+	Methods []string `json:"methods"`
+}
+
+func newService(sd *protokit.ServiceDescriptor) *service {
+	methods := make([]string, len(sd.GetMethods()))
+	for i, md := range sd.GetMethods() {
+		methods[i] = md.GetName()
+	}
+
+	return &service{Name: sd.GetName(), Methods: methods}
 }
