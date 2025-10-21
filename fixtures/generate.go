47df112caf8cf2fbf659b4e8777dcdbc89779872
David Muto (pseudomuto) <david.muto@gmail.com>
2025-10-21 10:02:25 -0400

    Add support for protobuf editions
    - Add edition detection methods: GetEdition(), IsEditions(), GetEditionName()
    - Add GetEditionComments() for accessing edition directive comments
    - Add feature detection methods: HasExplicitFieldPresence(), GetSyntaxType()
    - Enhance IsProto3() methods to maintain backward compatibility
    - Add edition comment extraction in parser (field path 14)
    - Create test fixtures for edition 2023 and 2024 syntax
    - Add test coverage for editions parsing, services, enums, and messages
    - Update descriptor set generation to include edition files
    - Fix utils tests to account for additional proto files
    - Reorganize types.go to follow code structure conventions

--- fixtures/generate.go
+++ fixtures/generate.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate protoc --descriptor_set_out=fileset.pb --include_imports --include_source_info -I. ./booking.proto ./todo.proto ./extend.proto
+//go:generate protoc --descriptor_set_out=fileset.pb --include_imports --include_source_info -I. ./booking.proto ./todo.proto ./extend.proto ./edition2023.proto ./edition2024.proto ./edition2023_implicit.proto
